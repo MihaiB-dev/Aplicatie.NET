6b1390645456aedc550a67bb4f033456e9c279d6
MihaiB-dev <mihaibivol.dev@gmail.com>
2023-11-24 13:38:11 +0200

    create .NET Core project

---
+++ App.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
