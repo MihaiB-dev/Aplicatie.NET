90c630c79127066c9cd9e2b7ece48e23f128f8ab
Alezuu <alexandrazugravu3@gmail.com>
2023-11-30 12:55:03 +0200

    user login

--- App.NET/Program.cs
+++ App.NET/Program.cs
@@ -13,7 +13,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+  
 
+// hei
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
